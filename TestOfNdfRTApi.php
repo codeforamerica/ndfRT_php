98660b0fd0a092fde7effc9b6ecdcaff6f7ea532
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-08-05 16:50:24 -0400

    Added tests from example use

--- TestOfNdfRTApi.php
+++ TestOfNdfRTApi.php
@@ -20,6 +20,24 @@ class TestOfApiClass extends UnitTestCase {
     function testApiConstructs(){
     	$this->api = new self::$class_name();
     	$this->check_class_params('_http _root api_url');
+	$this->assertIsA($this->api->getAllInfo('N0000152900'), 'string');
+	$this->assertIsA($this->api->getAssociationList(), 'string');
+	$this->assertIsA($this->api->getRelatedConceptsByAssociation('N0000152900','Product_component'), 'string');
+	$this->assertIsA($this->api->getChildConcepts('N0000022046','true'), 'string');
+	$this->assertIsA($this->api->findConceptsByName('morphine','DRUG_KIND'), 'string');
+	$this->assertIsA($this->api->findConceptsByID('RXCUI',161), 'string');
+	$this->assertIsA($this->api->findDrugInteractions('N0000145914',2), 'string');
+	$this->assertIsA($this->api->interactions('N0000007070','N0000179514',2), 'string');
+	$this->assertIsA($this->api->getKindList(), 'string');
+	$this->assertIsA($this->api->getRelatedConceptsByRole('N0000145914','has_PE','false'), 'string');
+	$this->assertIsA($this->api->getParentConcepts('N0000153235','false'), 'string');
+	$this->assertIsA($this->api->getPropertyList(), 'string');
+	$this->assertIsA($this->api->getConceptsByProperty('RxNorm_CUI',161), 'string');
+	$this->assertIsA($this->api->getRelatedConceptsByReverseRole('N0000000478','may_treat','false'), 'string');
+	$this->assertIsA($this->api->getRoleList(), 'string');
+	$this->assertIsA($this->api->getTypeList(), 'string');
+	$this->assertIsA($this->api->getVAClassOfConcept('N0000160729'), 'string');
+	$this->assertIsA($this->api->getVersion(), 'string');
     }
 
     function check_class_params($params=NULL,$mode=TRUE){
